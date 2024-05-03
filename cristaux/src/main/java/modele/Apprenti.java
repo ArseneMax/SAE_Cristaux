ca5051eaf028f796f6fd636af6179909481d88e9
ArseneMax <arsenemaxencezs@gmail.com>
2024-05-03 15:02:27 +0200

    Modification de la classe Apprenti et Temple (ajout d'un champ de la classe Cristal)

--- cristaux/src/main/java/modele/Apprenti.java
+++ cristaux/src/main/java/modele/Apprenti.java
@@ -2,8 +2,10 @@ package modele;
 
 public class Apprenti {
     private Position position ;
+    private Cristal cristal;
 
     public Apprenti(){
         position = new Position(0,0);
+        cristal = null;
     }
 }
--- cristaux/src/main/java/modele/Temple.java
+++ cristaux/src/main/java/modele/Temple.java
@@ -5,9 +5,10 @@ import javafx.scene.paint.Paint;
 public class Temple implements ConstantesCanvas{
     private Position position;
     private Paint couleur;
-
-    public Temple(Position pos, int idCouleur){
+    private Cristal cristal;
+    public Temple(Position pos, int idCouleur, Cristal parCristal){
         position = pos;
         couleur = COULEURS_TEMPLES[idCouleur];
+        cristal = parCristal;
     }
 }
