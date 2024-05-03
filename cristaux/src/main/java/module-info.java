80f6cee23bbfd30d7bb962eade9bd18dee319099
Alexis <kalambay.dinanga@gmail.com>
2024-05-03 14:15:55 +0200

    Création du dossier

---
+++ cristaux/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.cristaux {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.cristaux to javafx.fxml;
+    exports com.example.cristaux;
+}
